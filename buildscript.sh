627100b4867d33e1576f9430106a4925e0af9489
ziegler <ziegler@enpziegler-mac>
2024-09-16 12:13:47 -0400

    Added build script

---
+++ buildscript.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -f
+
+OS=$(uname)
+
+# set up environment
+classPath="$PWD/lib/*"
+
+# install local jar
+mvn install:install-file -Dfile=lib/clas12detector-dc-1.0-SNAPSHOT.jar -DgroupId=org.jlab.clas12.detector -DartifactId=clas12detector-dc -Dversion=1.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=coat-libs-11.0.2-SNAPSHOT.jar -DgroupId=org.jlab.coat -DartifactId=coat-libs -Dversion=11.0.2-SNAPSHOT -Dpackaging=jar
+mvn install
