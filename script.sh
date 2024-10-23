7bf456da4f32920d475b05bc580e3c11d0e300ca
Florian Hauenstein <hauenst@jlab.org>
2024-10-23 09:24:04 -0400

    Add jars to classpath in starting script

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 OS=$(uname)
 
 # set up environment
-classPath="$PWD/target/*:$PWD/target/classes/*"
+classPath="$PWD/target/*:$PWD/target/classes/*:lib/clas12detector-dc-1.0-SNAPSHOT.jar:lib/coat-libs-11.0.2-SNAPSHOT.jar"
 
 # run 
-java -Xmx4g -Xms1024m  -Dsun.java2d.pmoffscreen=false -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties -cp $classPath org.clas.detector.clas12calibration.viewer.Driver
+java -Xmx4g -Xms1024m  -Dsun.java2d.pmoffscreen=false  -cp $classPath org.clas.detector.clas12calibration.viewer.Driver
