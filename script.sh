75e5958c89934cf6c0a0e701cd5cca48c1d26b7c
ziegler <ziegler@enpziegler-mac>
2025-01-08 10:48:42 -0500

    memory allocation

--- script.sh
+++ script.sh
@@ -6,6 +6,5 @@ OS=$(uname)
 classPath="$PWD/target/*:$PWD/target/classes/*"
 
 # run 
-#java -Xmx32g -Xms24g  -Dsun.java2d.pmoffscreen=false -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties -cp $classPath org.clas.detector.clas12calibration.viewer.Driver
 java -Xmx8g -Xms4g  -Dsun.java2d.pmoffscreen=false -Djava.util.logging.config.file=$CLAS12DIR/etc/logging/debug.properties -cp $classPath org.clas.detector.clas12calibration.viewer.Driver
 
